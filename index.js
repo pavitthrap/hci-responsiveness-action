4a228f1b9d7266253e8b76f460c229bd7feda5d2
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-04-20 19:42:43 -0700

    add repo name input

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ var Github = require('github-api');
 
 function run () {
     const userToken  = core.getInput('repo-token');
+    const repoName = core.getInput('repo-name');
     var github = new Github({
         'token': userToken
     });
     var user = github.getUser();
-    var userRepos = user.listRepos(function(err, repos){
-        console.log('Number of repos: ' + userRepos.length);
-        userRepos.forEach(function(repo) {
-            console.log(repo);
-        })
-    });
+    var userRepo = Github.getRepo(user, repoName);
+    console.log('name of repo: ' + repoName);
+    console.log('userRepo: ' + userRepo);
+    console.log('type: ' + typeof(userRepo));
 }
 run();
