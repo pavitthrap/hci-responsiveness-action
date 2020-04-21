bd2c43c94df65c341059bdc1757a7b0084db3212
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-04-20 18:54:27 -0700

    try to create authenticated user

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var core = require('@actions/core');
+var Github = require('github-api');
 
+function run () {
+    const userToken  = core.getIntput('repo-token');
+    var github = new Github({
+        'token': userToken
+    });
+    var repoUser = github.getUser();
+    console.log('success?');
+}
 
+run();
